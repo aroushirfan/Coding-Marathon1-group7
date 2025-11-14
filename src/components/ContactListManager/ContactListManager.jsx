5093fcea630ecfd12644163a3a6ee9012ae7d9cf
sachin bhandari <bhandari.sachin.fi@gmail.com>
2025-11-14 14:45:20 +0200

    update

--- src/components/ContactListManager/ContactListManager.jsx
+++ src/components/ContactListManager/ContactListManager.jsx
@@ -1,77 +1,75 @@
-import React, { useState} from 'react';
-import Contact from "./Contact";
+import React, { useState } from "react";
 import "./ContactListManager.css";
 
-
 function ContactListManager() {
-
   const [contacts, setContacts] = useState([]);
-  const [form, setForm] = useState({name :"" ,job_title :"",birthday :"",notes :"",website : "",favorite : false,})
-  setForm({ ...form, [name]: type === "checkbox" ? checked : value});
-  
-  
+  const [form, setForm] = useState({
+    name: "",
+    job_title: "",
+    birthday: "",
+    notes: "",
+    website: "",
+    favorite: false,
+  });
+
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
-    setForm({ 
-      ...form, 
-      [name]: type === "checkbox" ? checked : value 
+    setForm({
+      ...form,
+      [name]: type === "checkbox" ? checked : value,
     });
   };
-  
+
   const addContact = (e) => {
     e.preventDefault();
     setContacts([...contacts, { id: Date.now(), ...form }]);
   };
-  return(
-   <div>
-      <h2> Contact List  </h2>
+  return (
+    <div>
+      <h2> Contact List </h2>
 
       <form onSubmit={addContact}>
-        
         <input
-        name ="name"
-        placeholder ="name"
-        value = {form.name}
-        onChange = {handleChange}
+          name="name"
+          placeholder="name"
+          value={form.name}
+          onChange={handleChange}
         />
         <input
-        name ="job_title"
-        placeholder ="Job Title"
-        value = {form.job_title}
-        onChange = {handleChange}
+          name="job_title"
+          placeholder="Job Title"
+          value={form.job_title}
+          onChange={handleChange}
         />
         <input
-        name ="birthday"
-        placeholder ="date"
-        value = {form.birthday}
-        onChange = {handleChange}
+          name="birthday"
+          placeholder="date"
+          value={form.birthday}
+          onChange={handleChange}
         />
         <input
-        name ="website"
-        placeholder ="website"
-        value = {form.website}
-        onChange = {handleChange}
+          name="website"
+          placeholder="website"
+          value={form.website}
+          onChange={handleChange}
         />
         <input
-        name ="notes"
-        placeholder ="Notes"
-        value = {form.notes}
-        onChange = {handleChange}
+          name="notes"
+          placeholder="Notes"
+          value={form.notes}
+          onChange={handleChange}
         />
         <input
-        name ="favorite"
-        placeholder ="checkbox"
-        checked = {form.favorite}
-        onChange = {handleChange}
+          name="favorite"
+          placeholder="checkbox"
+          checked={form.favorite}
+          onChange={handleChange}
         />
-      
-      <button>Add me</button>
-    </form>
 
-
+        <button>Add me</button>
+      </form>
     </div>
-  )
-  
+  );
 }
- 
-export default ContactListManager
+
+export default ContactListManager;
